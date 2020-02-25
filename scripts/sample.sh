a221e50a69e5714736ab65f8c579f5dd6cc6cc83
JackyLobsterD <jqduan@gmail.com>
2020-02-25 14:08:36 -0500

    trying to add scripts

--- scripts/sample.sh
+++ scripts/sample.sh
@@ -1,2 +1,12 @@
-#!/bin/sh
-ls
+#!/bin/bash
+#sample scripting
+
+
+variable ="Hello"
+echo $variable
+echo "what is your name?"
+read name
+echo "How do you do, $name?"
+read remark
+echo "I am $remark too!"
+
