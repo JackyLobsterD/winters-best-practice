dde4e74992ab205324d3a67f70433863a34db8b9
JackyLobsterD <jqduan@gmail.com>
2020-02-25 13:35:00 -0500

    修改目录结构

---
+++ serverside/index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    req.on('end', () => {
+        res.writeHead(200, {"content-type":"text/html"});
+        res.end('<form method="POST"><input name="test" /><input type="submit"></form>');
+    });
+    console.log('start to write');
+    var writeStream = fs.createWriteStream('./package.json');
+
+    req.pipe(writeStream)
+
+});
+
+const port=3001;
+console.log(port+' started!');
+server.listen(port)
