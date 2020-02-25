c65f3d6aa9bb63b99af4181d45a1c6cd5618514a
JackyLobsterD <jqduan@gmail.com>
2020-02-25 15:37:14 -0500

    add python script file add command

--- scripts/sample.py
+++ scripts/sample.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python3
 
-print('Hello World!')
+import subprocess
+
+print('1. git fetch')
+print('2. git pull')
+print('3. git status')
+print('8. run serverside')
+print('9. run all servers')
+value = input("Choose one above\n")
+print(f'You entered {value}')
+
+if (value=='1'):
+    subprocess.call(["git", "fetch"])
+
+elif (value=='2'):
+    subprocess.call(["git", "pull"])
+
+elif (value=='3'):
+    subprocess.call(["git", "status"])
+
+elif (value=='8'):
+    subprocess.call(["node", "../server/serverside/index.js"])
+
+else:
+    print('Not exist')
+
