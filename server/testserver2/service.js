c45e14b48cf8a695ac839407903c0107b8dcb934
JackyLobsterD <jqduan@gmail.com>
2020-03-03 17:11:01 -0500

    update testserver2

--- server/testserver2/service.js
+++ server/testserver2/service.js
@@ -3,6 +3,10 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
+    req.on('end', () => {
+        res.writeHead(200, {"content-type":"text/html"});
+        res.end('<form method="POST"><input name="test" /><input type="submit"></form>');
+    });
     const pathname = req.url;
     console.log(pathname);
     res.write('hello nodejs');
