9c0beb35ed5f10d0adcbdebc6c71b82642474779
JackyLobsterD <jqduan@gmail.com>
2020-03-03 17:02:02 -0500

    update testserver2

--- server/testserver2/service.js
+++ server/testserver2/service.js
@@ -1,4 +1,6 @@
 const http = require('http');
+console.log('testserver2 started, listening to port 8001\n http://3.12.119.235:8001/');
+
 
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
