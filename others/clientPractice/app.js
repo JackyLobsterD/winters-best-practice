ab54ed877c70bcbff698014f3cc3e5d4b7ceb476
JackyLobsterD <jqduan@gmail.com>
2020-03-03 17:08:51 -0500

    update testserver2

---
+++ others/clientPractice/app.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const fs = require('fs');
+const testFolder = './templates/';
+
+
+// fs.readFile('./package.json', function (err, data) {
+//     if (err) throw err;
+//     console.log(data);
+// });
+
+// var readStream = fs.createReadStream('./package.json')
+//
+// readStream.on('open', function () {
+//     console.log('open')
+// });
+
+
+const options = {
+    hostname: '3.12.119.235',
+    port: 8001,
+    path: '/',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+};
+
+console.log('req');
+const req = http.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`);
+    // res.on('data', d => {
+    //     process.stdout.write(d)
+    // })
+});
+
+// readStream.pipe(req)
+// req.pipe(readStream)
+
+
+req.on('error', error => {
+    if(error)throw error;
+});
+req.end();
--- server/testserver2/service.js
+++ server/testserver2/service.js
@@ -1,12 +1,13 @@
 const http = require('http');
-console.log('testserver2 started, listening to port 8001\n http://3.12.119.235:8001/');
 
 
-http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
     const pathname = req.url;
     console.log(pathname);
     res.write('hello nodejs');
     res.end();
-}).listen(8001);
+});
 
+console.log('testserver2 started, listening to port 8001\n http://3.12.119.235:8001/');
+server.listen(8001);
