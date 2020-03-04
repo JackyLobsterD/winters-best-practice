f3baead8c6822ce0bdefca4eea133748ebb597fa
JackyLobsterD <jqduan@gmail.com>
2020-03-04 18:03:30 -0500

    update testserver2

--- server/testserver2/service.js
+++ server/testserver2/service.js
@@ -43,18 +43,21 @@ const server = http.createServer((req, res) => {
         if (err) {
             console.log(404);
             console.log(staticFolder + page404);
-            fs.readFile(staticFolder + page404, (err, data) => {
-                if (err) throw err;
+            fs.readFile(staticFolder + page404, (error404, data404) => {
+                if (error404) throw error404;
                 res.writeHead(404, {'Content-Type': 'text/html;charset="utf-8"'});
                 console.log('write head');
-                res.write(data);
+                res.write(data404);
                 console.log('write data');
                 res.end();
             });
+            console.log('end error');
+        }else{
+            console.log('no error');
+            res.write(data);
+            res.end();
         }
-        console.log('no error');
-        res.write(data);
-        res.end();
+
     });
     // req.on('end', () => {
     //     res.writeHead(200, {"content-type":"text/html"});
