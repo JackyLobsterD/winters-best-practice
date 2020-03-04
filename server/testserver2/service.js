8e4dfc2914ac7ce5e47d88334ff2c6f95f2dbb55
JackyLobsterD <jqduan@gmail.com>
2020-03-04 17:32:02 -0500

    update testserver2

--- server/testserver2/service.js
+++ server/testserver2/service.js
@@ -12,20 +12,18 @@ const server = http.createServer((req, res) => {
     // console.log(result.query);
 
     if (pathname === '/') {
-
+        res.write('<h1>Hello</h1>Node.js is working');
     }
 
     if (pathname === '/favicon.ico') {
         fs.readFile(static + pathname, (err, data) => {
             if (err) throw err;
             res.write(data);
-            res.end();
         });
     }
 
 
-    // res.write('<h1>Hello</h1>Node.js is working');
-    // res.end();
+    res.end();
     // req.on('end', () => {
     //     res.writeHead(200, {"content-type":"text/html"});
     //     res.end('<form method="POST"><input name="test" /><input type="submit"></form>');
