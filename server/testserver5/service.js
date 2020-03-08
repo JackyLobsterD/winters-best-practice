109f28d34153b3547deaee9a047372c8f3f89f54
JackyLobsterD <jqduan@gmail.com>
2020-03-07 20:36:06 -0500

    update

--- server/testserver5/service.js
+++ server/testserver5/service.js
@@ -2,23 +2,24 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.writeHead(200, {'Content-Type': 'text/plain'});
     const pathname = req.url;
     console.log(pathname);
     // console.log(req);
-    let contentDisposition = req.headers['Content-Disposition']
+    console.log(req.headers);
+    let contentDisposition = req.headers['Content-Disposition'];
     console.log(contentDisposition);
 
 
     req.on('end', () => {
-        res.writeHead(200, {"content-type":"text/html"});
+        res.writeHead(200, {"content-type": "text/html"});
         res.end('<form method="POST"><input name="test" /><input type="submit"></form>');
     });
     console.log('start to write');
     var writeStream = fs.createWriteStream('./package.json');
-    req.pipe(writeStream)
+    req.pipe(writeStream);
 });
 
-const port=3001;
-console.log(port+' started!');
-server.listen(port,() => console.log(`Example app listening on port ${port}\n'+'http://3.12.119.235:${port}/`));
+const port = 3001;
+console.log(port + ' started!');
+server.listen(port, () => console.log(`Example app listening on port ${port}\n'+'http://3.12.119.235:${port}/`));
